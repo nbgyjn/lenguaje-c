83d171037b3b78d4ad195cb98eca458908f24f5e
nbgyjn <pablo1567@gmail.com>
2018-03-22 10:55:08 -0300

    asd

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero1;
+    float numero2;
+    numero2 = 5.55;
+    numero1 = 50;
+
+
+    printf("el numero entero es : %d y el numero flotante es : %.2f",numero1,numero2);
+    return 0;
+}
